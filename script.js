d56c1886fb76866fc71539d37198148b084f5db8
MuhdArifRawi <muhdarifrawi@gmail.com>
2020-03-26 00:21:47 +0800

    added delay when setting collapse to show.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toggleFunction(name){
     // $("#langCollapse").collapse('toggle');
     $(".collapse").collapse("hide");
+    setTimeout(function(){openSelected(name)},360);
+}
+
+function openSelected(name){
     selection = "#" + name;
     console.log(selection);
     $(selection).collapse("toggle");
 }
-
