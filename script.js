271ab523be37ea6ed5bea589eb9b8fafea41fe93
MuhdArifRawi <muhdarifrawi@gmail.com>
2020-03-26 00:04:54 +0800

    managed to implement hide all to all collapse class.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleFunction(name){
     // $("#langCollapse").collapse('toggle');
-
+    $(".collapse").collapse("hide");
     selection = "#" + name;
     console.log(selection);
     $(selection).collapse("toggle");
