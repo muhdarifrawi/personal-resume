f15a003f9b0a112ab842c512eec66d7cd99dc736
MuhdArifRawi <muhdarifrawi@gmail.com>
2020-03-26 19:39:57 +0800

    added color changer to skill buttons.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 $("#langCollapse").collapse('show');
+changeBtnColor();
+$("button[name='langCollapse']").addClass("btn btn-dark m-1");
+
+
+function changeBtnColor(){
+    $("button").removeClass("btn btn-dark m-1");
+    $("button").addClass("btn btn-light m-1");
+}
+
+
 
 function toggleFunction(name){
     // $("#langCollapse").collapse('toggle');
     $(".collapse").collapse("hide");
+    changeBtnColor();
     setTimeout(function(){openSelected(name)},360);
 }
 
 function openSelected(name){
+    btnSelection = "button" + "[name=" + name + "]";
+    console.log(btnSelection);
+    $(btnSelection).addClass("btn btn-dark m-1");
     selection = "#" + name;
     console.log(selection);
     $(selection).collapse("toggle");
